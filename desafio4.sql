e925931b5c1a5fcaaa276d8ca146e8b8b1e2cbdd
Bruno Trindade <brunotrndd@gmail.com>
2022-01-12 12:19:24 -0300

    req 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,1 +1,7 @@
--- starting
+SELECT DISTINCT us.username AS usuario,
+IF (MAX(YEAR(ss.play_date)) = 2021, 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
+FROM SpotifyClone.users AS us
+INNER JOIN SpotifyClone.users_songs AS ss
+ON us.user_id = ss.user_id
+GROUP BY us.user_id
+ORDER BY us.username;
